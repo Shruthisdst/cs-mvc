0347e43601068a471588e86fc84aa8498a57cc25
Sriranga <soori_rozee@yahoo.com>
2019-08-26 11:48:46 +0530

    Contact detail updated

--- application/views/flat/2-About/3-Contact_details/index.php
+++ application/views/flat/2-About/3-Contact_details/index.php
@@ -1,26 +1,20 @@
 <h1>Contact Information</h1>
-<h2>&nbsp;</h2>
-
-<h3>Change in email domain name</h3>
-<p>The domain part of the email address of all email addresses used by the office of Indian Academy of Sciences, including those of the staff, the journals, various programmes, and Current Science, has changed from 'ias.ernet.in' (or 'academy.ias.ernet.in') to 'ias.ac.in'. Thus, for example, the address office@ias.ernet.in (or office@academy.ias.ernet.in) has changed to office@ias.ac.in (without the string 'academy'). Please take note of this change.</p>
-<h4>For all manuscript submissions and queries</h4>
-
+<br />
+<h2>For all manuscript submissions and queries</h2>
 <p>
 	<strong>Current Science</strong><br />
 	C.V. Raman Avenue<br />
-	Post Box No. 8001<br />
-	Bengaluru - 560 080<br />
-	INDIA<br />
-	<strong>Tel.:</strong> +91 80 2266 1263<br />
+	Post Box No. 8001, Bengaluru - 560 080, INDIA<br />
+	<strong>Tel.</strong> : +91 80 2266 1263<br />
 	<strong>Fax:</strong> +91 80 2361 6094<br />
-	<strong>Email:</strong> currsci@ias.ac.in
+	<strong>E-mail</strong>currsci@ias.ac.in
 </p>
 
-<h4>For subscriptions and administration</h4>
+<h2>For subscriptions and administration</h2>
 <p>orders@ias.ac.in</p>
 
-<h4>For advertisements and reprints</h4>
+<h2>For advertisements and reprints</h2>
 <p>csc@ias.ac.in</p>
 
-<h4>For Media Contact</h4>
+<h2>For Media Contact</h2>
 <p>Press queries/Subscribe or unsubscribe to the Press Release, write to<br />cs-media@ias.ac.in</p>
