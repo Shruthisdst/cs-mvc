3c18ff82ef476b99b507babd61f21d8a44dffa4a
Sriranga <soori_rozee@yahoo.com>
2019-08-26 12:19:13 +0530

    Subscription details update

--- application/views/flat/2-About/3-Subscribe/index.php
+++ application/views/flat/2-About/3-Subscribe/index.php
@@ -1,68 +1,58 @@
 <h1>Subscription Information</h1>
-<h2>&nbsp;</h2>
-
-<h3>For India</h3>
+<p>Subscription Information (effective from January 2019)</p><br />
+<h2>For India</h2>
 <div class="row">
-	<div class="col-sm-6">
+	<div class="col-sm-6 gap-above-med">
 		<p>
 			<strong>Universities, Institutions and Government Departments</strong><br />
-			One Year: Rs 5000<br />
-			Two years: Rs 9500<br />
-			Three years: Rs 13000<br />
-			Five years: Rs 20000 
+			One Year: Rs 7000/-<br>
+			Three years: Rs 18000/-<br>
+			Five years: Rs 28000/-
 		</p>
 	</div>
-	<div class="col-sm-6">
+	<div class="col-sm-6 gap-above-med">
 		<p>
 			<strong>Industries, Business Houses and Commercial Establishments</strong><br />
-			One Year: Rs 12500<br />
-			Two years: Rs 20000<br />
-			Three years: Rs 30000<br />
-			Five years: Rs 50000 
+			One Year: Rs 15000/-<br>
+			Three years: Rs 40000/-<br>
+			Five years: Rs 60000/-
 		</p>
 	</div>
-</div>
-<div class="row">
-	<div class="col-sm-6">
+	<div class="col-sm-6 gap-above-med">
 		<p>
 			<strong>Schools &amp; Colleges</strong><br />
-			One Year: Rs 2000<br />
-			Two years: Rs 3000<br />
-			Three years: Rs 5000<br />
-			Five years: Rs 8000 
+			One Year: Rs 2500/-<br />
+			Three years: Rs 7000/-<br />
+			Five years: Rs 10000/-
 		</p>
 	</div>
-	<div class="col-sm-6">
+	<div class="col-sm-6 gap-above-med">
 		<p>
 			<strong>Personal Subscriptions</strong><br />
-			Two years: Rs 1500<br />
-			Three years: Rs 2100<br />
-			Five years: Rs 3000 
+			Two years: Rs 3000/-<br />
+			Three years: Rs 4000/-<br />
+			Five years: Rs 6000/-
 		</p>
 	</div>
 </div><hr />
 
-<h3>Other countries*</h3>
+<h2>Other countries*</h2>
 <dl>
 	<dt>Institutions/Industries</dt>
-	<dd>US$ 300</dd>
+	<dd>US $300</dd>
 </dl>
 <dl>
 	<dt>Personal</dt>
-	<dd>US$ 100</dd>
+	<dd>US $100</dd>
 </dl>
-<p>*All overseas addresses are served by Air Mail</p>
-<hr />
-
-<p>Send in your subscriptions to:</p>
+<p>*All overseas addresses are served by Air Mail</p><br />
+<h2>Send in your subscriptions to:</h2>
 <h3>Circulation Department</h3>
 <p>
 	Indian Academy of Sciences<br />
 	C.V Raman Avenue<br />
-	Post Box No. 8005<br />
-	Bengaluru 560 080<br />
-	INDIA
-</p>
+	Post Box No. 8005, Bengaluru 560 080, INDIA
+</p><hr />
 <p>Allow four weeks from subscription realisation for shipments to begin.</p>
 <p>Payment method: Demand Drafts or Cheques favouring <strong>Current Science Association, Bengaluru,</strong> payable at Bengaluru</p>
 <p>Cheques drawn in Indian rupees must be payable on a bank in India, and must add bank charges of Rs. 80.</p>
